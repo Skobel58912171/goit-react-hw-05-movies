267a0a883a90299bb39a2c714c6079fb163a306a
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2023-02-05 20:00:15 +0000

    Deploying to gh-pages from @ Skobel58912171/goit-react-hw-05-movies@1d5b469e4e80c5bbfb90f0eb3db19b11739c0968 🚀

--- static/js/186.d09291ba.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t),r.d(t,{Reviews:function(){return c}});var n=r(184),c=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("p",{children:"Reviews"})})};t.default=c}}]);
-//# sourceMappingURL=186.d09291ba.chunk.js.map
